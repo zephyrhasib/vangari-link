fb22e7d072ec032f62cace8e9fe51c4fdff76bcd
an01jum <mahfujaanjum2121@gmail.com>
2026-01-20 13:41:40 +0600

    commitCookies

--- public/index.php
+++ public/index.php
@@ -2,6 +2,14 @@
 <?php
 session_start();
 
+
+if (!isset($_SESSION['user_id']) && isset($_COOKIE['remember_user_id'])) {
+    $_SESSION['user_id'] = (int)$_COOKIE['remember_user_id'];
+    $_SESSION['role'] = $_COOKIE['remember_role'] ?? '';
+    $_SESSION['name'] = $_COOKIE['remember_name'] ?? '';
+    $_SESSION['profile_pic'] = $_COOKIE['remember_profile_pic'] ?? '';
+}
+
 $url = $_GET['url'] ?? 'home/index';
 
 $parts = explode('/', trim($url, '/'));
